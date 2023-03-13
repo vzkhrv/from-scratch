604826efe45591507f85882c1b5e249407722d54
v.zaharov <v.zaharov@zarplata.ru>
2023-03-13 10:45:16 +0700

    Удалил все файлы

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest"
-  },
-  testEnvironment: "jsdom",
-  transformIgnorePatterns: ["node_modules"],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
--- spec/index.spec.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import { App } from "../src/App";
-
-it("проверка корневого узла", () => {
-  const view = render(<App />);
-
-  expect(view.getByText("App")).toBeInTheDocument();
-});
