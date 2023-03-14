a7acfb27b0da2c1eacae92d98ba60888548fa416
v.zaharov <v.zaharov@zarplata.ru>
2023-03-14 16:52:27 +0700

    Выделили юниты, запланировали тесты

--- spec/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-
-import { App } from '../src/App';
-
-it('Проверка корневого компонента', () => {
-    const view = render(<App />);
-
-    expect(view.getByText(/React/)).toBeInTheDocument();
-});
